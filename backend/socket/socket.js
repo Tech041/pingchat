b325f8d6d08b81406ba2b41c0ca824dc3681c0f6
nelson <chinwubanelson@gmail.com>
2025-04-07 06:22:31 +0100

    added client url to socket

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -5,7 +5,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://pingme-chat-app.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
