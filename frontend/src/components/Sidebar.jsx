7b9bfe24c4b07c05ec2a4a80a55f3b7dd3a186c4
nelson <chinwubanelson@gmail.com>
2025-04-09 11:44:54 +0100

    added welcome message

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import SearchInput from "./SearchInput";
 import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
 import useGetConversations from "../hooks/useGetConversations";
 import PingConversation from "./PingConversation";
+import { AppContext } from "../context/AppContext";
 
 const SideBar = () => {
   const { loading } = useGetConversations();
+  const { authUser } = useContext(AppContext);
 
   return (
     <div className=" h-screen  w-full  flex flex-col overflow-scroll relative">
+      <div className="flex items-center justify-center">
+        <div className="py-3">
+          <p className="text-center">Hello {authUser.userData.fullName} ❤️</p>
+          <p className="text-green-500 text-center">Welcome to Pingme</p>
+          <p className="text-center">Select a chat to start messaging</p>
+        </div>
+      </div>
       <div className=" w-full h-full px-4 mb-40 pb-20">
         <SearchInput />
         <div className="divider px-3"></div>
